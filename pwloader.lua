8d3588907f6524a611bb9d200f2813c4a460dc8e
Jupiterly <90882309+Jupiterly@users.noreply.github.com>
2022-06-20 10:21:10 -0500

    Create pwloader.lua

---
+++ pwloader.lua
@@ -0,0 +1,10 @@
+--//arsenal
+if game.PlaceId == 286090429 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Jupiterly/planet-ware/main/arsenal.lua"))()
+--// cb
+elseif game.PlaceId == 301549746 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Jupiterly/planet-ware/main/cb.lua"))()
+--// bb
+elseif game.PlaceId == 3233893879 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Jupiterly/planet-ware/main/bb.lua"))()
+end
